af90b39826844a656f973ea8548584fd86d64a89
Mario Calvo <mario.calvo@outlook.com>
2020-06-02 19:32:50 -0600

    populating DB

--- movies-api/services/userMovies.js
+++ movies-api/services/userMovies.js
@@ -1,23 +1,28 @@
-const MongoLib = require('../utils/mocks/mongoLib');
+const MongoLib = require('../lib/mongo');
 
-class UserMoviesService{
+class UserMoviesService {
   constructor() {
-    this.collection = 'user-movies',
-    this.mongoDB = new MongoLib()
+    (this.collection = 'user-movies'), (this.mongoDB = new MongoLib());
   }
-  async getUserMovies({userId}){     
-    const query = userId && {userId}
-    const userMovies = await this.mongoDB.getAll(this.collection, query)
-    return userMovies || []
+  async getUserMovies({ userId }) {
+    const query = userId && { userId };
+    const userMovies = await this.mongoDB.getAll(this.collection, query);
+    return userMovies || [];
   }
-  async createUserMovie({userMovie}){
-    const createUserMovieId = await this.mongoDB.create(this.collection, userMovie)
-    return createUserMovieId
+  async createUserMovie({ userMovie }) {
+    const createUserMovieId = await this.mongoDB.create(
+      this.collection,
+      userMovie
+    );
+    return createUserMovieId;
   }
-  async deleteUserMovie({userMovieId}){
-    const deleteUserMovieId = await this.mongoDB.delete(this.collection, userMovieId)
-    return deleteUserMovieId
+  async deleteUserMovie({ userMovieId }) {
+    const deleteUserMovieId = await this.mongoDB.delete(
+      this.collection,
+      userMovieId
+    );
+    return deleteUserMovieId;
   }
 }
 
-module.exports = UserMoviesService
+module.exports = UserMoviesService;
