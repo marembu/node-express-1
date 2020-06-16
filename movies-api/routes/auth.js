db7beaa88af7b0b8df1715f4ac7cf42b3bdb8ac6
Mario Calvo <mario.calvo@outlook.com>
2020-06-15 21:34:06 -0600

    rewriting sign in route

--- movies-api/routes/auth.js
+++ movies-api/routes/auth.js
@@ -1,54 +1,118 @@
+// const express = require('express');
+// const passport = require('passport');
+// const boom = require('@hapi/boom');
+// const jwt = require('jsonwebtoken');
+// const ApiKeysService = require('../services/apiKeys');
+// const { config } = require('../config/index');
+// //Basic strategy
+// require('../utils/auth/strategies/basic');
+
+// function authApi(app) {
+//   const router = express.Router();
+//   app.use('/api/auth', router);
+
+//   const apiKeysService = new ApiKeysService();
+
+//   router.post('/sign-in', async function (req, res, next) {
+//     const { apiKeyToken } = req.body;
+//     if (!apiKeyToken) {
+//       next(boom.unauthorized('apiKeyToken is required'));
+//     }
+
+//     passport.authenticate('basic', function (error, user) {
+//       try {
+//         if (error || !user) {
+//           next(boom.unauthorized());
+//         }
+//         req.login(user, { session: false }, async function (error) {
+//           if (error) {
+//             next(error);
+//           }
+//           const apiKey = await apiKeysService.getApiKey({ token: apiKeyToken });
+//           if (!apiKey) {
+//             next(boom.unauthorized());
+//           }
+//           const { _id: id, name, email } = user;
+//           const payload = {
+//             sub: id,
+//             name,
+//             email,
+//             scopes: apiKeyToken.scopes,
+//           };
+//           const token = jwt.sign(payload, config.authJwtSecret, {
+//             expiresIn: '15m',
+//           });
+
+//           return res.status(200).json({ token, user: { id, name, email } });
+//         });
+//       } catch (error) {
+//         next(error);
+//       }
+//     })(req, res, next);
+//   });
+// }
+
+// module.exports = authApi;
+
 const express = require('express');
-const passport = require('passport');
-const boom = require('@hapi/boom');
-const jwt = require('jsonwebtoken');
 const ApiKeysService = require('../services/apiKeys');
-const { config } = require('../config/index');
-//Basic strategy
+const boom = require('@hapi/boom');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/');
+//requiriendo la basic strategy
 require('../utils/auth/strategies/basic');
 
 function authApi(app) {
   const router = express.Router();
-  app.use('/api/auth', router);
-
-  const apiKeysService = new ApiKeysService();
+  app.use('/api/auth/', router);
 
   router.post('/sign-in', async function (req, res, next) {
-    const { apiKeyToken } = req.body;
+    const [apiKeyToken] = req.body;
     if (!apiKeyToken) {
-      next(boom.unauthorized('apiKeyToken is required'));
+      next(boom.unauthorized('Missing Api Key Token'));
     }
-
-    passport.authenticate('basic', function (error, user) {
+    passport.authenticate('basic', async function (error, user) {
       try {
         if (error || !user) {
           next(boom.unauthorized());
         }
-        req.login(user, { session: false }, async function (error) {
+
+        passport.login(user, { session: false }, async function (error) {
           if (error) {
             next(error);
           }
-          const apiKey = await apiKeysService.getApiKey({ token: apiKeyToken });
+          const apiKeyService = new ApiKeysService();
+          const apiKey = await apiKeyService.getApiKey({ token: apiKeyToken });
           if (!apiKey) {
             next(boom.unauthorized());
           }
-          const { _id: id, name, email } = user;
+          const { _id: id, email, name } = user;
+
           const payload = {
             sub: id,
-            name,
             email,
-            scopes: apiKeyToken.scopes,
+            name,
+            scopes: apiKey.scopes,
           };
+
           const token = jwt.sign(payload, config.authJwtSecret, {
             expiresIn: '15m',
           });
 
-          return res.status(200).json({ token, user: { id, name, email } });
-        });
+          res.status(200).json({
+            token,
+            user: {
+              id,
+              name,
+              email,
+            },
+          });
+        })(req, res, next);
       } catch (error) {
         next(error);
       }
-    })(req, res, next);
+    });
   });
 }
 
