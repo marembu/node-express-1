2a49aa3c73b74f9036cfdca897eebcb81a362a62
Mario Calvo <mario.calvo@outlook.com>
2020-06-24 23:38:44 -0600

    aouth.js has errors

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+//BASIC Strategy
 require("./utils/auth/strategies/basic");
+
+//
+require("./utils/auth/strategies/oauth");
 
 app.post("/auth/sign-in", async function (req, res, next) {
   const { rememberMe } = req.body;
@@ -98,6 +102,31 @@ app.delete("/user-movies/:userMovieId", async function (req, res, next) {
   res.status(status).json({ message: data.message });
   //el mensaje debe ser el mismo que devuelve la api: user movie deleted
 });
+
+app.get(
+  "/auth/google-oauth",
+  passport.authenticate("google-oauth", {
+    scope: ["email", "profile", "openid"],
+  })
+);
+
+app.get(
+  "/auth/google-oauth/callback",
+  passport.authenticate("google-oauth", {
+    session: false,
+  }),
+  async function (req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+    const { token, ...user } = req.user;
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev,
+    });
+    res.status(200).json(user);
+  }
+);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
